04682074a1f52c51e1a968182a982b2796999dd1
Dylan Agyemang <agdylan@longleaf-login5.its.unc.edu>
2024-04-10 12:44:25 -0400

    Initial Commit: Added main.py and imported necessary code

---
+++ main.py
@@ -0,0 +1,8 @@
+import icon_registration.data
+import icon_registration.networks as networks
+from icon_registration.config import device
+
+import numpy as np
+import torch
+import torchvision.utils
+import matplotlib.pyplot as plt
